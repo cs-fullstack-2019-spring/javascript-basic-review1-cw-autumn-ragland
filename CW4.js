45823712498852ec99ccfba1041be536d7609460
autumn-ragland <raglanda998@gmail.com>
2019-01-11 11:50:36 -0600

    Bonus added

--- CW4.js
+++ CW4.js
@@ -3,8 +3,25 @@
 // Log the message FTEMP Fahrenheit equals CTEMP in Celcius
 // Use the following line of code for the conversion (this uses a Math function which we will discuss more in future lessons):
 // var CTEMP = Math.round((FTEMP - 32) * 5 / 9);
+
+
+
+//Without Bonus
+
+// var FTEMP = parseInt(prompt("Enter the temperature!"));
+// var CTEMP = Math.round((FTEMP - 32) * 5 / 9);
+//
+// console.log(FTEMP + " degrees fahrenheit equals " + CTEMP + " degrees in celcius");
+
+
+
 // BONUS: Keep prompting until the User actually enters a number (HINT: Look into using NaN)
 
-var FTEMP = parseInt(prompt("Enter the temperature!"));
+do
+{
+    var FTEMP = parseInt(prompt("Enter the temperature!"));
+}
+while (isNaN(FTEMP));
 var CTEMP = Math.round((FTEMP - 32) * 5 / 9);
 console.log(FTEMP + " degrees fahrenheit equals " + CTEMP + " degrees in celcius");
+
--- CW5.js
+++ CW5.js
@@ -3,10 +3,26 @@
 // Prompts the User to enter the number of months in which they want to pay off the loan (NUMMONTHS)
 // Calculates the monthly payment they need to make and
 // logs it to the console as To pay of a loan of LOANAMT in NUMMONTHS months it will take MATH per month
+
+
+
+//Without Bonus
+
+// var LOANAMT = parseInt(prompt("Enter your outstanding loan amount"));
+// var NUMMONTHS = parseInt(prompt("Enter the number of months in which you want to pay off the loan"));
+// var MATH = LOANAMT/NUMMONTHS;
+// console.log("To pay off the a loan of $" + LOANAMT + " in " + NUMMONTHS + " months you must pay $" + MATH + " per month.");
+
+
+
 // BONUS: Keep prompting until the User actually enters a number for both loan amount
 // and number of months (HINT: Look into using NaN)
 
-var LOANAMT = parseInt(prompt("Enter your outstanding loan amount"));
-var NUMMONTHS = parseInt(prompt("Enter the number of months in which you want to pay off the loan"));
+do
+{
+    var LOANAMT = parseInt(prompt("Enter your outstanding loan amount"));
+    var NUMMONTHS = parseInt(prompt("Enter the number of months in which you want to pay off the loan"));
+}
+while (isNaN(LOANAMT) && isNaN(NUMMONTHS));
 var MATH = LOANAMT/NUMMONTHS;
 console.log("To pay off the a loan of $" + LOANAMT + " in " + NUMMONTHS + " months you must pay $" + MATH + " per month.");
